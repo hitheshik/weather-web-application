8219dfe914682939e7bdd683a4c0bdd360274438
hitheshi <kariyawasamhitheshi16@gmail.com>
2026-02-11 17:34:18 +0530

    Refactor App component layout and enhance SearchBox styling

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -42,8 +42,12 @@ function App(){
   };
 
   return(
-    <>
-    </>
+    <div className = "bg-[#eee] flex flex-col items-center px-5">
+      <div className = "flex w-full items-center justify-between">
+        <h1 className="text-blue-500 text-2xl font-bold mt-6">Weather Forecast</h1> 
+        <SearchBox/>
+      </div>  
+    </div>
   );
 }
 
