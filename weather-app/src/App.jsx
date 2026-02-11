f16897f7d29c7ea235a98a4fc60bc1695ab49313
hitheshi <kariyawasamhitheshi16@gmail.com>
2026-02-11 17:09:03 +0530

    Implement weather fetching functionality and state management in App component

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,10 +1,50 @@
-function App() {
-  
-  return (
-    <>
+import {useEffect, useState} from 'react';
+import axios from "axios";
+
+function App(){
+  const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const apiKey = "63220e6adf11231ffb394e753ba8a95";
+
+  const fetchWeather = async(city) => {
+    try{
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=Delhi&appid=${apiKey}&units=metric`
+      );
+
+      const data = res.data;
+      const today = data.list[0];
+      console.log(data);
+
+      setWeather({
+        city: data.city.name,
+        temp: today.main.temp,
+        desc: today.weather[0].description,
+        humidity: today.main.humidity,
+        wind: today.wind.speed,
+      });
+
+      const daily = data.list
+        .filter((item) => item.dt_txt.includes("12:00:00"))
+        .map((day) => ({
+          temp: Math.round(day.main.temp),
+          desc: day.weather[0].description,
+          icon: day.weather[0].icon
+        }));
       
+      setForecast(daily);
+
+    } catch(error){
+      console.error(error);
+      alert("City Not Found");
+    }
+  };
+
+  return(
+    <>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
